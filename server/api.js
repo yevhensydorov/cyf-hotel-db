11c9fb594a37d593b505371021d4e80d4134cc8e
dgrabov <d.grabov@gmail.com>
2017-09-23 19:57:54 +0100

    Add exercises 6 to 9

--- server/api.js
+++ server/api.js
@@ -41,6 +41,52 @@ router.post('/reservations', function(req, res) {
   res.status(200).end();
 });
 
+router.get('/reservations', function(req, res) {
+  // TODO read req.query.name or req.query.id to look up reservations and return
+  res.status(200).json({
+    reservations:[{
+      title: 'Mr',
+      firstname: 'Laurie',
+      surname: 'Ainley',
+      email: 'laurie@ainley.com',
+      roomId: 1,
+      checkInDate: '2017-10-10',
+      checkOutDate: '2017-10-17'
+    }]
+  });
+});
+
+router.get('/reservations/date-from/:dateFrom', function(req, res) {
+  // TODO read req.params.dateFrom to look up reservations and return
+  res.status(200).json({
+    reservations:[{
+      title: 'Mr',
+      firstname: 'Laurie',
+      surname: 'Ainley',
+      email: 'laurie@ainley.com',
+      roomId: 1,
+      checkInDate: '2017-10-10',
+      checkOutDate: '2017-10-17'
+    }, {
+      title: 'Miss',
+      firstname: 'Someone',
+      surname: 'Else',
+      email: 'someone@else.com',
+      roomId: 2,
+      checkInDate: '2017-11-12',
+      checkOutDate: '2017-11-15'
+    }]
+  });
+});
+
+router.put('/invoice', function(req, res) {
+  // TODO read req.query.reservationId and req.body.invoice and insert into DB
+  res.status(200).end();
+});
+
+
+
+
 
 
 
