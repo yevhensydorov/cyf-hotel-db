b3aee6ef481b19a38f384f8ffe1489290fd6683e
dgrabov <d.grabov@gmail.com>
2017-09-24 09:55:18 +0100

    implemented ex 8

--- server/api.js
+++ server/api.js
@@ -102,7 +102,10 @@ router.get('/reservations/date-from/:dateFrom', function(req, res) {
 
 router.put('/invoice', function(req, res) {
   // TODO read req.query.reservationId and req.body.invoice and insert into DB
-  res.status(200).end();
+  res.status(200).json({
+    reservationId: req.query.reservationId,
+    invoice: req.body.invoice
+  });
 });
 
 module.exports = router;
