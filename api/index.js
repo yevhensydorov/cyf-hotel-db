02e73262813cca2427342691ed89ec31b589f210
Pedro <ams.pedro@gmail.com>
2018-02-08 20:04:55 +0000

    Simplified the repo.

--- api/index.js
+++ api/index.js
@@ -1,22 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const customers = require("./customers");
-const discount = require("./discount");
-const invoice = require("./invoice");
-const reservations = require("./reservations");
-const reviews = require("./reviews");
-const roomTypes = require("./room-types");
 const class2 = require("./class2");
-
-router.use("/customers", customers);
-router.use("/discount", discount);
-router.use("/invoice", invoice);
-router.use("/reservations", reservations);
-router.use("/reviews", reviews);
-router.use("/room-types", roomTypes);
-router.use("/api", class2);
+router.use("/", class2);
 
 module.exports = router;
-
-// router.use("/v2", v2ApiController);
--- api/reviews.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/reviews", (req, res) => {
-  // TODO read req.body.review
-  res.status(200).json(req.body);
-});
-
-module.exports = router;
